b3e8cc60373016782f66301f1909fc227f1aaa40
Christopher Kennedy <kennedysgarage@gmail.com>
2012-04-15 12:55:16 -0400

    fixed search overlay

--- place-list-client/Spotify/coca/js/client.js
+++ place-list-client/Spotify/coca/js/client.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   var $mainContainer = $("#main-container");
   var $eventList = $("#list-events");
   var $trackList = $("#tracks");
-  var $searchList = $("#event-search");
+  var $searchList = $("#search-content");
   var $addEventButton = $("#add-event-btn");
   var $addFacebookButton = $("#add-facebook-btn");
   var $addTrackField = $("#add-track");
