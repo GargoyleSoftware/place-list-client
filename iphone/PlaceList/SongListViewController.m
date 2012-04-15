6eca0d226d1bd1c628b03a435af7be034e02825b
David Young-Chan Kay <davidykay@gmail.com>
2012-04-14 22:43:02 -0400

    Integrated new cells in song list.

--- iphone/PlaceList/SongListViewController.m
+++ iphone/PlaceList/SongListViewController.m
@@ -41,6 +41,8 @@
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
 }
 
 - (void)viewDidUnload
